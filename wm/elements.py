5efd1dcc1f16936e06222b7f49e10c2802e3108a
Atheriel <atheriel@gmail.com>
2013-11-25 21:15:09 -0500

    Updated for the new dict-like behaviour.
    Signed-off-by: Atheriel <atheriel@gmail.com>

--- wm/elements.py
+++ wm/elements.py
@@ -18,13 +18,10 @@ class AccessibleApplication(object):
         self._windows = []
 
         # Gets the windows
-        try:
-            num_windows = self._element.count('AXWindows')
-            if num_windows != 0:
-                for ref in self._element.get('AXWindows'):
-                    self._windows.append(AccessibleWindow(ref, self))
-        except Exception as e:
-            logging.debug('Error while fetching windows for bundle <%s>: %s', bundle, e.args[0])
+        num_windows = self._element.count('AXWindows')
+        if num_windows != 0:
+            for ref in self._element['AXWindows']:
+                self._windows.append(AccessibleWindow(ref, self))
 
     @property
     def bundle(self):
@@ -33,25 +30,26 @@ class AccessibleApplication(object):
     @property
     def title(self):
         if self._title is None:  # Cache the title, since it won't change
-            try:
-                self._title = self._element.get('AXTitle')
-            except Exception as e:
-                logging.debug('No title found for bundle %s. Exception: %s.', self._bundle, e.args[0])
+            if 'AXTitle' in self._element:
+                self._title = self._element['AXTitle']
+            else:
+                logging.debug('No title found for bundle %s.', self._bundle)
 
         return self._title
 
     @property
     def hidden(self):
-        try:
-            _hidden = self._element.get('AXHidden')
-            return _hidden
-        except Exception as e:
-            logging.debug('No AXHidden property found for bundle %s. Exception: %s.', self._bundle, e.args[0])
+        if 'AXHidden' in self._element:
+            return self._element['AXHidden']
+        else:
             return None
 
     @hidden.setter
     def hidden(self, value):
-        self._element.set('AXHidden', value)
+        if 'AXHidden' in self._element and self._element.can_set('AXHidden'):
+            self._element['AXHidden'] = value
+        else:
+            logging.debug('Could not set application with bundle %s as (un)hidden.', self._bundle)
 
 
 class AccessibleWindow(object):
@@ -64,29 +62,33 @@ class AccessibleWindow(object):
 
     @property
     def position(self):
-        try:
-            _pos = self._element.get('AXPosition')
-            return _pos
-        except Exception as e:
-            logging.debug('No AXPosition property found for window in app %s. Exception: %s.', self._parent.title, e.args[0])
+        if 'AXPosition' in self._element:
+            return self._element.get('AXPosition')
+        else:
+            logging.debug('No AXPosition property found for window in app %s.', self._parent.title)
             return None
 
     @position.setter
     def position(self, value):
-        self._element.set('AXPosition', value)
+        if 'AXPosition' in self._element and self._element.can_set('AXPosition'):
+            self._element['AXPosition'] = value
+        else:
+            logging.debug('Could not set AXPosition property found for window in app %s.', self._parent.title)
 
     @property
     def size(self):
-        try:
-            _size = self._element.get('AXSize')
-            return _size
-        except Exception as e:
-            logging.debug('No AXSize property found for window in app %s. Exception: %s.', self._parent.title, e.args[0])
+        if 'AXSize' in self._element:
+            return self._element['AXSize']
+        else:
+            logging.debug('No AXSize property found for window in app %s.', self._parent.title)
             return None
 
     @size.setter
     def size(self, value):
-        self._element.set('AXSize', value)
+        if 'AXSize' in self._element and self._element.can_set('AXSize'):
+            self._element['AXSize'] = value
+        else:
+            logging.debug('Could not set AXSize property found for window in app %s.', self._parent.title)
 
     @property
     def frame(self):
@@ -109,11 +111,10 @@ class AccessibleWindow(object):
 
     @property
     def minimized(self):
-        try:
-            _min = self._element.get('AXMinimized')
-            return _min
-        except Exception as e:
-            logging.debug('No AXMinimized property found for window in app %s. Exception: %s.', self._parent.title, e.args[0])
+        if 'AXMinimized' in self._element:
+            return self._element['AXMinimized']
+        else:
+            logging.debug('No AXMinimized property found for window in app %s.', self._parent.title)
             return None
 
 
@@ -125,12 +126,11 @@ def new_application(pid, bundle):
     app = None
     ref = acbl.create_application_ref(pid)
     try:
-        role = ref.get('AXRole')
-        if role == u'AXApplication':
+        if ref['AXRole'] == u'AXApplication':
             app = AccessibleApplication(ref, bundle)
             logging.debug('Bundle <%s> is an accessible application.', bundle)
         else:
-            logging.debug('Bundle <%s> is not an accessible application, role is %s.', bundle, role)
+            logging.debug('Bundle <%s> is not an accessible application, role is %s.', bundle, ref['AXRole'])
     except acbl.APIDisabledError:
         logging.debug('Bundle <%s> is not available to the Accessibility API.', bundle)
     except Exception as e:
@@ -166,8 +166,7 @@ def get_accessible_applications(ignored_bundles = []):
         # Create AXUIElementRef
         ref = acbl.create_application_ref(application.processIdentifier())
         try:
-            role = ref.get('AXRole')
-            if role == u'AXApplication':
+            if ref['AXRole'] == u'AXApplication':
                 running_apps.append(AccessibleApplication(ref, application.bundleIdentifier()))
                 _accessibile.append(application.bundleIdentifier())
             else:
