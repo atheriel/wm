3acdf3fec8b91e0f5d24ebfbd3e165b482801dc3
Atheriel <atheriel@gmail.com>
2013-12-30 22:00:07 -0500

    More robust platform check for header files.
    Signed-off-by: Atheriel <atheriel@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from distutils.core import setup, Extension
+from platform import mac_ver
 
 import wm
 
+# Deal with the new location of headers in Mavericks
+if mac_ver()[0].startswith('10.9'):
+    header_dir = '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/HIServices.framework/Versions/A/Headers'  # noqa
+else:
+    header_dir = '/System/Library/Frameworks/ApplicationServices.framework/Frameworks/HIServices.framework/Headers'
+
 accessibility = Extension('wm._accessibility',
     sources = ['wm/_accessibility.c'],
-    include_dirs = ['/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/HIServices.framework/Versions/A/Headers'],  # noqa
-    # prior to mavericks:
-    # include_dirs = ['/System/Library/Frameworks/ApplicationServices.framework/Frameworks/HIServices.framework/Headers'],
+    include_dirs = [header_dir],
     # Uncomment the next line to include debug symbols while compiling
     # extra_compile_args = ['-g'],
     extra_link_args = ['-framework', 'ApplicationServices', '-v']
